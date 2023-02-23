d5c1e261209a162d815b3dd1b2b879c117311605
martinuropa16 <mark.martin.uropa@accenture.com>
2023-02-23 10:10:00 +0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskViewsComponent } from './pages/task-views/task-views.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: TaskViewsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskViewsComponent } from './pages/task-views/task-views.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskViewsComponent
   ],
   imports: [
     BrowserModule,
